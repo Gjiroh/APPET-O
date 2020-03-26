56391df1800e14c6d89d8adf901078aee96417c9
Gabriel <marmeloamare@gmail.com>
2020-03-26 17:20:17 -0300

    Testando o uso do git

--- app/src/main/java/com/peteleco/appet/CadastrarActivity.java
+++ app/src/main/java/com/peteleco/appet/CadastrarActivity.java
@@ -1,18 +1,30 @@
 package com.peteleco.appet;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class CadastrarActivity extends AppCompatActivity {
 
     private Button cadastrar;
-    private EditText nome, senha, grr, email, cpf;
+    private static final String TAG = "CadastrarActivity";
+    private EditText campo_nome, campo_senha, campo_grr, campo_email, campo_cpf, campo_telefone;
 
     private FirebaseAuth mAuth;
 
@@ -21,27 +33,161 @@ public class CadastrarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar);
 
+        mAuth = FirebaseAuth.getInstance();
+
         cadastrar = findViewById(R.id.btCadastro);
         cadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                nome = findViewById(R.id.editNome);
-                String pegarNome = nome.getText().toString();
+                campo_nome = findViewById(R.id.editNome);
+                String pegarNome = campo_nome.getText().toString();
 
-                senha = findViewById(R.id.editSenha);
-                String pegarSenha = senha.getText().toString();
+                campo_senha = findViewById(R.id.editSenha);
+                String pegarSenha = campo_senha.getText().toString();
 
-                grr = findViewById(R.id.editGRR);
-                String pegarGRR = grr.getText().toString();
+                campo_grr = findViewById(R.id.editGRR);
+                String pegarGRR = campo_grr.getText().toString();
 
-                email = findViewById(R.id.editEmail);
-                String pegarEmail = email.getText().toString();
+                campo_email = findViewById(R.id.editEmail);
+                String pegarEmail = campo_email.getText().toString();
 
-                cpf = findViewById(R.id.editCPF);
-                String pegarCPF = cpf.getText().toString();
+                campo_cpf = findViewById(R.id.editCPF);
+                String pegarCPF = campo_cpf.getText().toString();
+
+                campo_telefone = findViewById(R.id.editTel);
+                String pegarTelefone = campo_telefone.getText().toString();
+
+                createAccount(pegarEmail, pegarSenha, pegarGRR, pegarNome, pegarCPF, pegarTelefone);
 
             }
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser user) {
+//        hideProgressBar();
+        if (user != null) {
+//            mStatusTextView.setText(getString(R.string.emailpassword_status_fmt,
+//                    user.getEmail(), user.isEmailVerified()));
+//            mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+
+//            findViewById(R.id.emailPasswordButtons).setVisibility(View.GONE);
+//            findViewById(R.id.emailPasswordFields).setVisibility(View.GONE);
+//            findViewById(R.id.signedInButtons).setVisibility(View.VISIBLE);
+
+            if (user.isEmailVerified()) {
+//                findViewById(R.id.verifyEmailButton).setVisibility(View.GONE);
+            } else {
+//                findViewById(R.id.verifyEmailButton).setVisibility(View.VISIBLE);
+            }
+        } else {
+//            mStatusTextView.setText(R.string.signed_out);
+//            mDetailTextView.setText(null);
+
+//            findViewById(R.id.emailPasswordButtons).setVisibility(View.VISIBLE);
+//            findViewById(R.id.emailPasswordFields).setVisibility(View.VISIBLE);
+//            findViewById(R.id.signedInButtons).setVisibility(View.GONE);
+
+        }
+    }
+
+    private boolean validateForm(String email, String password, String nome, String grr, String cpf, String telefone) {
+        // TODO: validação dos dados do usuário
+        // Exemplo: senha com 8 caracteres, e-mail contém @, etc
+
+//        String email = mEmailField.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            campo_email.setError("Este campo é obrigatório");
+            campo_email.requestFocus();
+            return false;
+        } else {
+            campo_email.setError(null);
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            campo_senha.setError("Este campo é obrigatório");
+            campo_senha.requestFocus();
+            return false;
+        } else {
+            campo_senha.setError(null);
+        }
+        return true;
+    }
+
+    private void createAccount(String email, String password, String nome, String grr, String cpf, String telefone) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm(email, password, nome, grr, cpf, telefone)) {
+            return;
+        }
+//        showProgressBar();
+        try {
+            // [START create_user_with_email]
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                updateUI(user);
+                                Toast.makeText(CadastrarActivity.this, " Cadastro realizado com sucesso! ",
+                                        Toast.LENGTH_SHORT).show();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                try {
+                                    throw task.getException();
+                                } catch (FirebaseAuthWeakPasswordException e) {
+                                    campo_senha.setError("A senha deve ter pelo menos 6 caracteres!");
+                                    campo_senha.requestFocus();
+                                } catch (FirebaseAuthInvalidCredentialsException e) {
+                                    campo_email.setError("Verifique se o endereço de e-mail está correto");
+                                    campo_email.requestFocus();
+                                } catch (FirebaseAuthUserCollisionException e) {
+                                    campo_email.setError("Usuário já existe");
+                                    campo_email.requestFocus();
+                                } catch (Exception e) {
+                                    Log.e(TAG, e.getMessage());
+                                    Toast.makeText(CadastrarActivity.this, "Autenticação falhou. Procure ajuda! ",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+
+                                updateUI(null);
+                            }
+
+                            // [START_EXCLUDE]
+//                        hideProgressBar();
+                            // [END_EXCLUDE]
+                        }
+                    });
+            // [END create_user_with_email]
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // desloga o usuário
+    private void signOut() {
+        mAuth.signOut();
+        updateUI(null);
+    }
+
+    private void showProgressBar() {
+        //TODO: mostrar barra de progresso
+    }
+
+    private void hideProgressBar() {
+        //TODO: esconder barra de progresso
     }
 }
--- app/src/main/java/com/peteleco/appet/MainActivity.java
+++ app/src/main/java/com/peteleco/appet/MainActivity.java
@@ -4,15 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button irCadastrar, logar, esqSenha, enter;
     private EditText login;
     private EditText senha;
+
+    private static final String TAG = "MainActivity";
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,5 +73,96 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void signIn(String email, String password) {
 
+        Log.d(TAG, "signIn:" + email);
+
+        if (!validateForm(email, password)) {
+            return;
+        }
+//        showProgressBar();
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+
+                            // Sign in success, update UI with the signed-in user's information
+
+                            Log.d(TAG, "signInWithEmail:success");
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            updateUI(user);
+
+                        } else {
+
+                            // If sign in fails, display a message to the user.
+
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+
+                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+
+                                    Toast.LENGTH_SHORT).show();
+
+                            updateUI(null);
+
+                            // [START_EXCLUDE]
+
+                            checkForMultiFactorFailure(task.getException());
+
+                            // [END_EXCLUDE]
+
+                        }
+
+
+                        // [START_EXCLUDE]
+
+                        if (!task.isSuccessful()) {
+
+                            mStatusTextView.setText(R.string.auth_failed);
+
+                        }
+
+                        hideProgressBar();
+
+                        // [END_EXCLUDE]
+
+                    }
+
+                });
+
+        // [END sign_in_with_email]
+
+    }
+
+    private boolean validateForm(String login, String senha) {
+        // TODO: validação dos dados do usuário
+        // Exemplo: senha com 8 caracteres, e-mail contém @, etc
+
+//        String email = mEmailField.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            campo_email.setError("Este campo é obrigatório");
+            campo_email.requestFocus();
+            return false;
+        } else {
+            campo_email.setError(null);
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            campo_senha.setError("Este campo é obrigatório");
+            campo_senha.requestFocus();
+            return false;
+        } else {
+            campo_senha.setError(null);
+        }
+        return true;
+    }
 }
