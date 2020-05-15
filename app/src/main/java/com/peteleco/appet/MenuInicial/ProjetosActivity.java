208e525f1fa9b0719377c63fa55190ee9e032bbe
jp-turra <jp-turra@users.noreply.github.com>
2020-05-15 17:03:20 -0300

    Adicionando Menu na Tela de Projetos

--- app/src/main/java/com/peteleco/appet/MenuInicial/ProjetosActivity.java
+++ app/src/main/java/com/peteleco/appet/MenuInicial/ProjetosActivity.java
@@ -6,7 +6,11 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.peteleco.appet.MenuInicial.ProjetosAdapter.AdapterProjetos;
 import com.peteleco.appet.MenuInicial.ProjetosModel.ModeloProjetos;
@@ -19,11 +23,33 @@ public class ProjetosActivity extends AppCompatActivity {
 
     private RecyclerView mostrarProjetos;
     private List<ModeloProjetos> listProjetos = new ArrayList<>();
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_projetos, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.item:
+                Toast.makeText(this, "Item selecionado", Toast.LENGTH_SHORT).show();
+
+            case R.id.itemItem2:
+                Toast.makeText(this, "Item selecionado", Toast.LENGTH_SHORT).show();
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_projetos);
-
         mostrarProjetos = findViewById(R.id.RecyclerViewProjetos);
 
         // Listar projetos
